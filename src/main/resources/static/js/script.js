a5140c6895da3dc0b9f6bba04d54ae984ac4e9be
manuolivera <emanuelli_oliveira@estudante.sc.senai.br>
2021-06-14 08:36:38 -0300

    S.A

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,21 @@
+let path_selected
+let id_selected
+
+
+const showModalDeletar = function(path,id){
+    path_selected = path
+    id_selected = id
+	$('#modalDeletar').modal('show')
+
+}
+
+const deletar = function(){
+    if(path_selected != "" && id_selected != ""){
+        window.location.href='/' + path_selected + '/delete/' + id_selected
+
+    }
+
+}
+
+
+
